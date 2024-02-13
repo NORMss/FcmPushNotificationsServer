c9f552f832c54cf8e8a94d674629c191b4303853
NORMss <serega.b301@gmail.com>
2024-02-13 22:51:01 +0700

    feat: server has been created for communication between devices

---
+++ src/main/kotlin/com/plcoding/Application.kt
@@ -0,0 +1,29 @@
+package com.plcoding
+
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import com.plcoding.plugins.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureMonitoring()
+    configureRouting()
+
+    val serviceAccountStream =
+        this::class.java.classLoader.getResourceAsStream("service_account_key.json")
+
+    val options = FirebaseOptions.builder()
+        .setCredentials(GoogleCredentials.fromStream(serviceAccountStream))
+        .build()
+
+    FirebaseApp.initializeApp(options)
+}
---
+++ src/main/kotlin/com/plcoding/plugins/Routing.kt
@@ -0,0 +1,11 @@
+package com.plcoding.plugins
+
+import com.plcoding.sendNotification
+import io.ktor.server.application.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    routing {
+        sendNotification()
+    }
+}
